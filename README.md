889ccd8a99b1693a928e5382b67defd0f511985b
gimjin <gimjin@qq.com>
2023-05-10 05:04:04 +0000

    fix: 类似compositionstart事件中，单次可输入多个字符时无法转换缺陷

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
-# 半角
-
-用中文输入法写代码，全角自动转半角符号。
+### 全角自动转半角【，转 [,
 
 ![例子](https://gimjin.github.io/banjiao-cdn/example.gif)
-
-# 映射表
 
 ```JSON
 [
--- extension.js
+++ extension.js
@@ -4,17 +4,27 @@ const keymapsId = 'banjiao.keymaps'
 const switchId = 'banjiao.switch'
 let config, keymapsConfig, switchConfig
 
-function transformHalfChar (char) {
-  for (let index = 0; index < keymapsConfig.length; index++) {
-    if (keymapsConfig[index].full === char) {
-      return keymapsConfig[index].half
+function convertToHalfWidthChar (text) {
+  if (text.length === 1) {
+    for (let i = 0; i < keymapsConfig.length; i++) {
+      if (keymapsConfig[i].full === text) {
+        return keymapsConfig[i].half
+      }
     }
-  }
 
-  return char
+    return text
+  } else {
+    let newText = ''
+
+    for (let i = 0; i < text.length; i++) {
+      newText += convertToHalfWidthChar(text[i])
+    }
+
+    return newText
+  }
 }
 
-function replaceChar (event) {
+function textEditorEdit (event) {
   if (vscode.window.activeTextEditor === undefined) return
 
   let charRange
@@ -22,34 +32,47 @@ function replaceChar (event) {
   vscode.window.activeTextEditor.edit(
     editBuilder => {
       event.contentChanges.forEach(content => {
-        const char = content.text
-        let halfChar, prevChar
+        if (content.text.length === 1) {
+          const char = content.text
+          let halfChar, prevChar
 
-        if (content.range.start.character > 0) {
-          prevChar = event.document.getText(new vscode.Range(content.range.start.translate(0, -1), content.range.start))
-        }
+          if (content.range.start.character > 0) {
+            prevChar = event.document.getText(new vscode.Range(content.range.start.translate(0, -1), content.range.start))
+          }
 
-        // ^ 全角 …… 会触发两次change事件
-        if (char === '…' && char === prevChar) {
-          halfChar = transformHalfChar(char)
-          charRange = new vscode.Range(content.range.start.translate(0, -1), content.range.end.translate(0, 1))
-          editBuilder.replace(charRange, halfChar)
-        }
-
-        // _ 全角 —— 会触发两次change事件
-        if (char === '—' && char === prevChar) {
-          halfChar = transformHalfChar(char)
-          charRange = new vscode.Range(content.range.start.translate(0, -1), content.range.end.translate(0, 1))
-          editBuilder.replace(charRange, halfChar)
-        }
-
-        // 其他全角字符
-        if (char !== '…' && char !== '—') {
-          halfChar = transformHalfChar(char)
-
-          if (halfChar !== char) {
-            charRange = new vscode.Range(content.range.start, content.range.end.translate(0, 1))
+          // ^ 全角 …… 会触发两次change事件
+          if (char === '…' && char === prevChar) {
+            halfChar = convertToHalfWidthChar(char)
+            charRange = new vscode.Range(content.range.start.translate(0, -1), content.range.end.translate(0, 1))
             editBuilder.replace(charRange, halfChar)
+          }
+
+          // _ 全角 —— 会触发两次change事件
+          if (char === '—' && char === prevChar) {
+            halfChar = convertToHalfWidthChar(char)
+            charRange = new vscode.Range(content.range.start.translate(0, -1), content.range.end.translate(0, 1))
+            editBuilder.replace(charRange, halfChar)
+          }
+
+          // 其他全角字符
+          if (char !== '…' && char !== '—') {
+            halfChar = convertToHalfWidthChar(char)
+
+            if (halfChar !== char) {
+              charRange = new vscode.Range(content.range.start, content.range.end.translate(0, 1))
+              editBuilder.replace(charRange, halfChar)
+            }
+          }
+        } else {
+          // onDidChangeTextDocument 与 [compositionstart](https://developer.mozilla.org/zh-CN/docs/Web/API/Element/compositionstart_event)
+          // 性质一样, 中文输入法每按下一个子母都会触发, 所以会出现 content.text 是多个字符情况
+          const text = content.text
+          const newText = text.replaceAll('……', '…')
+          const textWithHalfChar = convertToHalfWidthChar(newText)
+
+          if (textWithHalfChar !== newText) {
+            charRange = new vscode.Range(content.range.start, content.range.end.translate(0, 1 + text.length - newText.length))
+            editBuilder.replace(charRange, textWithHalfChar)
           }
         }
       })
@@ -57,7 +80,7 @@ function replaceChar (event) {
     { undoStopBefore: false }
   ).then(fulfilled => {
     if (fulfilled === false) {
-      replaceChar(event)
+      textEditorEdit(event)
     }
   })
 }
@@ -90,7 +113,7 @@ function activate ({ subscriptions }) {
 
   vscode.workspace.onDidChangeTextDocument(event => {
     if (switchConfig) {
-      replaceChar(event)
+      textEditorEdit(event)
     }
   })
 
@@ -103,7 +126,7 @@ function activate ({ subscriptions }) {
     statusBarItem.tooltip = getStatusBarItemTooltip()
   })
 
-  console.info('activate banjiao.')
+  console.info('已激活半角插件')
 }
 
 function deactivate () {}
